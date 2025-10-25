41faf6c434cafca13c74ceef2e79a0480c504eac
Zanko Yaroslav <arikzanko9@gmail.com>
2025-10-25 20:04:57 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -3,13 +3,11 @@ from typing import Dict, List
 
 
 class Person:
-    people: Dict[str, "Person"] = {}
-
-    def __init__(self, name: str, age: int) -> None:
+    people = {}
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
         Person.people[name] = self
-
 
 def create_person_list(people: List[dict]) -> List[Person]:
 
